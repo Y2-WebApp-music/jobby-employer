9b9881e2b382cd0f4d8354bfbc6c1ea2ef6374eb
guynut <kunguy.159@gmail.com>
2026-01-16 16:26:58 +0700

    ✨ [JOB-75] add sidebar of shadcn

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -1,5 +1,7 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { Calendar, ChevronDown, Home, Inbox, Search, Settings } from "lucide-react";
 import JobbyLogo from "@/assets/icons/JobbyLogo.svg?react";
+
+import { RiFileList3Fill } from "react-icons/ri";
 
 import {
   Sidebar,
@@ -12,31 +14,32 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "../ui/collapsible";
 
 const items = [
   {
     title: "Home",
-    url: "#",
+    url: "test",
     icon: Home,
   },
   {
     title: "Inbox",
-    url: "#",
+    url: "sdjxfbnm",
     icon: Inbox,
   },
   {
     title: "Calendar",
-    url: "#",
+    url: "1234",
     icon: Calendar,
   },
   {
     title: "Search",
-    url: "#",
+    url: "98765",
     icon: Search,
   },
   {
     title: "Settings",
-    url: "#",
+    url: "hhhhhh",
     icon: Settings,
   },
 ];
@@ -48,23 +51,33 @@ export function AppSidebar() {
         <JobbyLogo height={45} />
       </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        <Collapsible defaultOpen className="group/collapsible">
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                <RiFileList3Fill />
+                Profile
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {items.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <a href={item.url}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
       </SidebarContent>
     </Sidebar>
   );
