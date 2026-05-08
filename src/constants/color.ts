ec1b1e88896b5903b5927610c9e15fb9bdcec39f
guynut <kunguy.159@gmail.com>
2026-05-08 14:33:39 +0700

    Add resume rendering and viewing components

---
+++ src/constants/color.ts
@@ -0,0 +1,30 @@
+export const resume_color = [
+  {
+    name: "Blue",
+    value: "var(--color-c-214785)",
+  },
+  {
+    name: "Red",
+    value: "var(--color-c-f45353)",
+  },
+  {
+    name: "Green",
+    value: "var(--color-c-42c64d)",
+  },
+  {
+    name: "Pink",
+    value: "var(--color-c-e86df6)",
+  },
+  {
+    name: "Orange",
+    value: "var(--color-c-fe8d36)",
+  },
+];
+
+export const resume_color_classes = [
+  { bg: "bg-c-214785", text: "text-c-214785", border: "border-c-214785" },
+  { bg: "bg-c-f45353", text: "text-c-f45353", border: "border-c-f45353" },
+  { bg: "bg-c-42c64d", text: "text-c-42c64d", border: "border-c-42c64d" },
+  { bg: "bg-c-e86df6", text: "text-c-e86df6", border: "border-c-e86df6" },
+  { bg: "bg-c-fe8d36", text: "text-c-fe8d36", border: "border-c-fe8d36" },
+];
